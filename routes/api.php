d7e89de72f2a2947354943a9d5014d961ece5a6d
leiyuxue <369568872@qq.com>
2020-09-25 05:39:09 +0000

    频率限制

--- routes/api.php
+++ routes/api.php
@@ -15,11 +15,17 @@ use Illuminate\Support\Facades\Route;
 */
 //测试路由
 Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function () {
-    // 短信验证码
-    Route::post('verificationCodes', 'VerificationCodesController@store')
-        ->name('verificationCodes.store');
+    Route::middleware('throttle'.config('api.rate_limits.sign'))->group(function(){
+        // 短信验证码
+        Route::post('verificationCodes', 'VerificationCodesController@store')
+            ->name('verificationCodes.store');
 //    用户注册
-    Route::post('users','UsersController@store')->name('users.store');
+        Route::post('users','UsersController@store')->name('users.store');
+    });
+    Route::middleware('throttle'.config('api.rate_limits.access'))->function(function(){
+
+    });
+
 });
 Route::prefix('v2')->name('api.v2.')->group(function(){
     Route::get('version',function(){
